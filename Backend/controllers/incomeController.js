a4724168a0bdc616d2c3e577ac0659fcc86f27cf
Abhishek692002 <abhi.contai2002@gmail.com>
2025-07-25 01:34:25 +0530

    Complete Expense Apis

--- Backend/controllers/incomeController.js
+++ Backend/controllers/incomeController.js
@@ -1,6 +1,6 @@
 const User = require("../models/user");
 const Income = require("../models/income");
-
+const xlsr = require("xlsx");
 //Add Income Source
 exports.addIncome = async (req, res) => {
     const userId = req.user.id;
@@ -28,8 +28,44 @@ exports.addIncome = async (req, res) => {
     }
 };
 //Get ALL Income Source
-exports.getAllIncome = async (req, res) => {};
+exports.getAllIncome = async (req, res) => {
+    const userId = req.user.id;
+    try {
+        const income = await Income.find({ userId }).sort({ date: -1 });
+        res.json(income);
+    } catch {
+        res.status(500).json({ message: "Server Error" });
+     }
+};
 //Delete Income Source
-exports.deleteIncome = async (req, res) => { };
+exports.deleteIncome = async (req, res) => {
+    try {
+        await Income.findByIdAndDelete(req.params.id);
+        res.json({message:"Income deleted successfully"})
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+ };
 //Download Excel
-exports.downloadedIncomeExcel = async (req, res) => { };
+exports.downloadedIncomeExcel = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const income = await Income.find({ userId }).sort({ date: -1 });
+
+        //Prepare data for Excel
+        const data = income.map((item) => ({
+            Source: item.source,
+            Amount: item.amount,
+            Date: item.date,
+        }));
+
+        const wb = xlsr.utils.book_new();
+        const ws = xlsr.utils.json_to_sheet(data);
+        xlsr.utils.book_append_sheet(wb, ws, "Income");
+        xlsr.writeFile(wb, 'income_details.xlsx');
+        res.download('income_details.xlsx');
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+ };
---
+++ Backend/routes/expenseRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+    addExpense,
+    getAllExpense,
+    deleteExpense,
+    downloadedExpenseExcel,
+} = require("../controllers/expenseController");
+const { protect } = require("../middleware/authMiddleware");
+const router = express.Router();
+
+router.post("/add", protect, addExpense);
+router.get("/get", protect, getAllExpense);
+router.get("/downloadexel", protect, downloadedExpenseExcel);
+router.delete("/:id", protect, deleteExpense);
+
+module.exports = router;
